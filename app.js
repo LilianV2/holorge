dba2463e6422b5682c119626e62cefbd8dd725d1
lilia <lilianom59@outlook.fr>
2022-12-12 14:22:06 +0100

    horloge

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-/*
-function bouge()
-
-{
-    let aiguilleSecondes = document.querySelector("#secondes");
-    let aiguilleMinutes = document.querySelector("#minutes");
-    let aiguilleHeures = document.querySelector("#heures");
-
-    let maDate = new Date();
-    let valeursec = maDate.getSeconds();
-    let valeurmin = maDate.getMinutes();
-    let valeurh = maDate.getHours();
-
-    console.log(valeursec);
-    console.log(valeurmin);
-    console.log(valeurh);
-
-    aiguilleSecondes.style.transform = "rotate(" + ((valeursec*6)-90) + "deg)";
-    aiguilleMinutes.style.transform = "rotate(" + ((valeurmin*6)-90) + "deg)";
-    aiguilleHeures.style.transform = "rotate(" + ((valeurh*30)-90) + "deg)";
-}
-
-window.setInterval(bouge, 1000);
- */
 
 // function horloge
 
